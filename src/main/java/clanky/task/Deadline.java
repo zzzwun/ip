f9f7572f30831421f9b9ac782e593ac100bdbab3
zzzwun <zhewenlau@gmail.com>
2026-09-17 21:49:32 +0800

    Added toSaveFormat methods

--- src/main/java/clanky/task/Deadline.java
+++ src/main/java/clanky/task/Deadline.java
@@ -18,4 +18,9 @@ public class Deadline extends Task{
     public String toString(){
         return super.toString() + " (by: " + by + ")";
     }
+
+    @Override
+    public String toSaveFormat() {
+        return super.toSaveFormat() + " | " + by;
+    }
 }
